00384377c996e677c278f22c281d267e5c199ec2
souzeraxp <160558783+souzeraxp@users.noreply.github.com>
2024-02-19 23:08:07 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function headericons() {
+let headericons = document.querySelector ('.mobile-menu')
+if (headericons.classlist.contains('open')) {
+    headericons.classlist.remove('open');
+    document.querySelector('.menu')
+} else { 
+    headericons.classList.add ('open');
+   } {
+
+
+}
+
+}
